245fe4b851fa3dccfac910a1c43bff017031b3bf
Erick Bauer <ebauer@gvdasa.com.br>
2022-12-22 20:42:48 -0300

    GVtalentos desafio 01 - desenvolver a classe DadoRPG.

---
+++ GVtalentosDesafio01/DadoRPG.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GVtalentosDesafio01
+{
+    class DadoRPG
+    {
+        public int QuantidadeDeLados { get; private set; }
+
+        public DadoRPG(int quantidadeDeLados)
+        {
+            QuantidadeDeLados = quantidadeDeLados;
+        }
+
+        public int RolarDado()
+        {
+            Random random = new Random();
+            return random.Next(1, this.QuantidadeDeLados + 1);
+        }
+    }
+}
--- GVtalentosDesafio01/Program.cs
+++ GVtalentosDesafio01/Program.cs
@@ -6,7 +6,26 @@ namespace GVtalentosDesafio01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DadoRPG d4 = new DadoRPG(4);
+            DadoRPG d6 = new DadoRPG(6);
+            DadoRPG d8 = new DadoRPG(8);
+            DadoRPG d10 = new DadoRPG(10);
+            DadoRPG d12 = new DadoRPG(12);
+            DadoRPG d20 = new DadoRPG(20);
+
+            PrintarResultadoRolarDado(d4, d4.RolarDado());
+            PrintarResultadoRolarDado(d6, d6.RolarDado());
+            PrintarResultadoRolarDado(d8, d8.RolarDado());
+            PrintarResultadoRolarDado(d10, d10.RolarDado());
+            PrintarResultadoRolarDado(d12, d12.RolarDado());
+            PrintarResultadoRolarDado(d20, d20.RolarDado());
+
+            Console.ReadLine();
+        }
+
+        static void PrintarResultadoRolarDado(DadoRPG dadoRPG, int numero)
+        {
+            Console.WriteLine($"O dado de {dadoRPG.QuantidadeDeLados} lados caiu o número {numero}");
         }
     }
 }
