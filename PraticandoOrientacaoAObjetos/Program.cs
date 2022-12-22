94b5eb061f4504d499eec4ae09515216cdd52339
Erick Bauer <ebauer@gvdasa.com.br>
2022-12-22 20:11:35 -0300

    Add project files.

---
+++ PraticandoOrientacaoAObjetos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PraticandoOrientacaoAObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
