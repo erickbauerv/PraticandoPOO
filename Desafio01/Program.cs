1f6ac151fddae469957a46132686ff1f2a1186e2
Erick Bauer <ebauer@gvdasa.com.br>
2022-12-23 17:32:07 -0300

    Desafio01 - Desenvolver uma classe DadoRPG.

---
+++ Desafio01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Desafio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DadoRPG d4 = new DadoRPG(4);
+            DadoRPG d6 = new DadoRPG(6);
+            DadoRPG d8 = new DadoRPG(8);
+            DadoRPG d10 = new DadoRPG(10);
+            DadoRPG d12 = new DadoRPG(12);
+            DadoRPG d20 = new DadoRPG(20);
+
+            PrintarResultadoRolarDado(d4, d4.RolarDado());
+            PrintarResultadoRolarDado(d6, d6.RolarDado());
+            PrintarResultadoRolarDado(d8, d8.RolarDado());
+            PrintarResultadoRolarDado(d10, d10.RolarDado());
+            PrintarResultadoRolarDado(d12, d12.RolarDado());
+            PrintarResultadoRolarDado(d20, d20.RolarDado());
+
+            Console.ReadLine();
+        }
+
+        static void PrintarResultadoRolarDado(DadoRPG dadoRPG, int numero)
+        {
+            Console.WriteLine($"O dado de {dadoRPG.QuantidadeDeLados} lados caiu o número {numero}");
+        }
+    }
+}
