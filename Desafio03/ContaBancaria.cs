cbb3d0aee806a29558a6ce86b114af3527280f1c
Erick Bauer <ebauer@gvdasa.com.br>
2022-12-30 13:09:03 -0300

    Desatio 03 - Histórico de transações de uma conta bancária

---
+++ Desafio03/ContaBancaria.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio03
+{
+    class ContaBancaria
+    {
+        public string Titular { get; set; }
+        public string CodigoConta { get; set; }
+        public List<string> ListaDeTranacacoes { get; set; } = new List<string>();
+        public double Saldo { 
+            get { return Saldo; } 
+            set 
+            {
+                if (value > 0)
+                {
+                    this.Saldo = value;
+                }
+                else
+                {
+                    Console.WriteLine("O valor a ser definido não pode ser menor que 0");
+                }
+            } 
+        }
+
+        public ContaBancaria(string titular, string codigoConta)
+        {
+            Titular = titular;
+            CodigoConta = codigoConta;
+            Saldo = 0;
+        }
+
+
+        public void Depositar(double valor)
+        {
+            this.Saldo += valor;
+            ListaDeTranacacoes.Add($"Foi depositado {valor}R$ na conta");
+        }
+
+        public void Sacar(double valor)
+        {
+            if (valor <= this.Saldo)
+            {
+                this.Saldo -= valor;
+                ListaDeTranacacoes.Add($"Foi sacado {valor}R$ da conta");
+            }
+            else
+            {
+                Console.WriteLine("Não foi possível fazer o saque");
+            }
+        }
+
+        public void ConsultarTransacoes()
+        {
+            foreach(string transacao in ListaDeTranacacoes)
+            {
+                Console.WriteLine(transacao);
+            }
+        }
+    }
+}
---
+++ Desafio03/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Desafio03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string menu = "0";
+            ContaBancaria contaBancaria = new ContaBancaria("Rogério", "8324098347194");
+
+            while (menu != "4")
+            {
+                Console.WriteLine("1 - Depositar\n2 - Sacar\n3 - Consultar transações\n4 - Sair");
+                menu = Console.ReadLine();
+                Console.Clear();
+
+                switch (menu)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o valor que será depositado");
+                        double valorSacar = double.Parse(Console.ReadLine());
+                        contaBancaria.Depositar(valorSacar);
+                        break;
+                    case "2":
+                        Console.WriteLine("Digite o valor que será sacado");
+                        double valorDepositar = double.Parse(Console.ReadLine());
+                        contaBancaria.Sacar(valorDepositar);
+                        break;
+                    case "3":
+                        contaBancaria.ConsultarTransacoes();
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
